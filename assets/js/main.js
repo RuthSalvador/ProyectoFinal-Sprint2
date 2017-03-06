59ab4d009fbe501f42a08103dbb27b9386e81445
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-03-06 16:53:47 -0500

    agregando Dropdown y una parte de botones

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+var mostarOcultar = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+  var todo = document.getElementById('todo')
+
+  	if (divtext.style.display == "block" ) {
+  		divtext.style.display = "none";
+
+  	} else{
+  		divtext.style.display = "block";
+  	}
+
+  var quiz1 = document.getElementById('quiz1');
+prueba1.onclick = function(){
+
+
+
+quiz1.innerHTML = "hola";
+}
+
+}
+
+
+
+/*var mostarOcultar = function(){
+  var text1 = document.getElementById('text1');
+  var text2 = document.getElementById('text2');
+  var text3 = document.getElementById('text3');
+
+  if (text1.style.display == "block"){
+    text1.style.display = "none";
+  } else if (text1.style.display == "none"){
+    text1.style.display = "block";
+
+  } else if (text2.style.display == "block"){
+    text2.style.display = "none";
+
+  } else if (text2.style.display == "none"){
+
+    text2.style.display = "block";
+
+}}*/
